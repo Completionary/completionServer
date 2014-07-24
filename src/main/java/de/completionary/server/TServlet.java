5da8d57fb9f9748676342b73684509cf6178df96
Jonas Kunze <kunze.jonas@gmail.com>
2014-07-24 22:50:59 +0200

    Made TServlet more basic: No bas64 encryption...

--- src/main/java/de/completionary/server/TServlet.java
+++ src/main/java/de/completionary/server/TServlet.java
@@ -1,18 +1,17 @@
 package de.completionary.server;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.thrift.TException;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TJSONProtocol;
@@ -20,74 +19,49 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TIOStreamTransport;
 import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
-import org.apache.thrift.transport.TTransportFactory;
 
 /**
- * <p>
- * A servlet for exposing Thrift services over HTTP. To use, create a subclass
- * that supplies a {@link TProcessor}. For example,
- * </p>
- * 
- * <pre>
- * 
- * 
- * 
- * public class CalculatorTServlet extends TServlet {
- * 
- *     public CalculatorTServlet() {
- *         super(new Calculator.Processor(new CalculatorHandler()));
- *     }
- * }
- * </pre>
+ * Servlet implementation class ThriftServer with a proper CORS implementation
  * 
  * <p>
- * TServlet expects to be called in one of three ways:
- * 
- * 1) Via HTTP POST with the request body being the plain binary protocol data.
- * 
- * 2) Via HTTP POST with the request body being url-safe base 64 (the client
- * should signal this with ?base64=true)
- * 
- * 3) In the JSONP style via HTTP GET with a "body" parameter containing
- * url-safe base 64, and a "callback" parameter with the function name to be
- * called. That is,
- * "some/url?body=<URL SAFE BASE64>&callback=<SOME JS CALLBACK>". (See
- * http://en.wikipedia.org/wiki/JSON#JSONP for more info on JSONP)
+ * This code is based on {@link gist.github.com/pieceable/430784}.
  * </p>
  * 
- * <p>
- * This code is based heavily on {@link gist.github.com/pieceable/430784}.
- * </p>
- * 
- * @author Jonas Kunze
+ * @author Jonas Kunze <kunze.jonas@gmail.com>
  */
 public class TServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -7641009085670384001L;
+    private static final long serialVersionUID = -9040416060293389629L;
 
-    protected TProcessor processor_ = null;
+    private final TProcessor processor;
 
-    protected TTransportFactory inputTransportFactory_ =
-            new TTransportFactory();
+    private final TProtocolFactory inProtocolFactory;
 
-    protected TTransportFactory outputTransportFactory_ =
-            new TTransportFactory();
+    private final TProtocolFactory outProtocolFactory;
 
-    protected TProtocolFactory inputProtocolFactory_ =
-            new TJSONProtocol.Factory();
+    private final Collection<Map.Entry<String, String>> customHeaders;
 
-    protected TProtocolFactory outputProtocolFactory_ =
-            new TJSONProtocol.Factory();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TServlet(
+            TProcessor processor,
+            TProtocolFactory inProtocolFactory,
+            TProtocolFactory outProtocolFactory) {
+        super();
+        this.processor = processor;
+        this.inProtocolFactory = inProtocolFactory;
+        this.outProtocolFactory = outProtocolFactory;
+        this.customHeaders = new ArrayList<Map.Entry<String, String>>();
+    }
 
-    private Base64 base64;
-
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public TServlet(
             TProcessor processor) {
-        processor_ = processor;
-
-        // We want to use URL-Safe mode, with no chunking.
-        base64 = new Base64(0, null, true);
+        this(processor, new TJSONProtocol.Factory(),
+                new TJSONProtocol.Factory());
     }
 
     @Override
@@ -100,136 +74,85 @@ public class TServlet extends HttpServlet {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST");
         response.setHeader("Access-Control-Max-Age", "1728000");
-
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept");
         // If our client sent any special header, we'd need to enumerate them here.
         // response.setHeader("Access-Control-Allow-Headers", "header1, header2, ...");
 
-        response.setHeader("Allow", "GET, POST, OPTIONS");
+        response.setHeader("Allow", "GET, OPTIONS");
     }
 
-    @Override
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        // If it's a GET, we'll assume the caller is trying to use the JSONP method.
-
-        InputStream in;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        // Our input comes base64 encoded from the 'body' get parameter
-        in =
-                new ByteArrayInputStream(base64.decode(request
-                        .getParameter("body")));
-
-        processRequest(in, out);
-
-        String jsCallback = request.getParameter("callback");
-        String responseAsBase64 = base64.encodeToString(out.toByteArray());
-
-        response.setContentType("application/javascript");
-
-        PrintStream ps = new PrintStream(response.getOutputStream());
-        ps.print(jsCallback + "(\"" + responseAsBase64 + "\");");
-        ps.flush();
-    }
-
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
     @Override
     protected void doPost(
             HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 
+        TTransport inTransport = null;
+        TTransport outTransport = null;
+
         response.setHeader("Access-Control-Allow-Origin", "*");
-
-        boolean useBase64 =
-                request.getParameter("base64") != null
-                        && request.getParameter("base64").equalsIgnoreCase(
-                                "true");
-
-        InputStream in;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        if (useBase64) {
-            byte[] postBody = readContentsOfStream(request.getInputStream());
-
-            postBody = base64.decode(postBody);
-            in = new ByteArrayInputStream(postBody);
-        } else {
-            in = request.getInputStream();
-        }
-
-        processRequest(in, out);
-
-        response.setContentType("application/x-thrift");
-
-        byte[] responseBytes;
-
-        if (useBase64) {
-            String responseAsBase64 = base64.encodeToString(out.toByteArray());
-
-            response.setContentType("text/plain");
-            responseBytes = responseAsBase64.getBytes();
-        } else {
-            response.setContentType("application/octet-stream");
-            responseBytes = out.toByteArray();
-        }
-
-        response.setContentLength(responseBytes.length);
-
-        OutputStream os = response.getOutputStream();
-        os.write(responseBytes);
-        os.close();
-    }
-
-    private byte[] readContentsOfStream(InputStream in) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        byte[] buffer = new byte[4096];
-        while (true) {
-            int bytesRead = in.read(buffer, 0, buffer.length);
-
-            if (bytesRead == -1) {
-                // end of input
-                break;
-            } else {
-                baos.write(buffer, 0, bytesRead);
-            }
-        }
-
-        return baos.toByteArray();
-    }
-
-    private void processRequest(InputStream in, OutputStream out) {
-        TTransport client = new TIOStreamTransport(in, out);
-        TProcessor processor = null;
-        TTransport inputTransport = null;
-        TTransport outputTransport = null;
-        TProtocol inputProtocol = null;
-        TProtocol outputProtocol = null;
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept");
 
         try {
-            processor = processor_;
-            inputTransport = inputTransportFactory_.getTransport(client);
-            outputTransport = outputTransportFactory_.getTransport(client);
-            inputProtocol = inputProtocolFactory_.getProtocol(inputTransport);
-            outputProtocol =
-                    outputProtocolFactory_.getProtocol(outputTransport);
+            response.setContentType("application/x-thrift");
 
-            while (processor.process(inputProtocol, outputProtocol)) {
+            if (null != this.customHeaders) {
+                for (Map.Entry<String, String> header : this.customHeaders) {
+                    response.addHeader(header.getKey(), header.getValue());
+                }
             }
-        } catch (TTransportException ttx) {
-            // Client died, just move on
-        } catch (TException tx) {
-            tx.printStackTrace();
-        } catch (Exception x) {
-            x.printStackTrace();
-        }
+            InputStream in = request.getInputStream();
+            OutputStream out = response.getOutputStream();
 
-        if (inputTransport != null) {
-            inputTransport.close();
-        }
+            TTransport transport = new TIOStreamTransport(in, out);
+            inTransport = transport;
+            outTransport = transport;
 
-        if (outputTransport != null) {
-            outputTransport.close();
+            TProtocol inProtocol = inProtocolFactory.getProtocol(inTransport);
+            TProtocol outProtocol =
+                    outProtocolFactory.getProtocol(outTransport);
+
+            processor.process(inProtocol, outProtocol);
+            out.flush();
+        } catch (TException te) {
+            throw new ServletException(te);
         }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    public void addCustomHeader(final String key, final String value) {
+        this.customHeaders.add(new Map.Entry<String, String>() {
+
+            public String getKey() {
+                return key;
+            }
+
+            public String getValue() {
+                return value;
+            }
+
+            public String setValue(String value) {
+                return null;
+            }
+        });
+    }
+
+    public void setCustomHeaders(Collection<Map.Entry<String, String>> headers) {
+        this.customHeaders.clear();
+        this.customHeaders.addAll(headers);
     }
 }
